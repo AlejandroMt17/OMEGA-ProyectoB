850c6ebfc604e9fa7839fe74c6508ca0d607d213
Angelo <tellezangelo800@outlook.com>
2026-05-21 22:34:29 -0600

    ptm

--- mobile/android/app/src/main/kotlin/com/omega/proyectob/proyecto_b/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.omega.proyectob.proyecto_b
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/lib/features/usuarios/business/usuario_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../data/models/usuario_model.dart';
-
-/// Contrato de acceso a datos del dominio **Usuario** (la UI depende de esto, no de HTTP).
-abstract class UsuarioRepository {
-  Future<List<UsuarioModel>> obtenerUsuarios();
-}
